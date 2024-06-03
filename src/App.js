9fa5795131ebabc328ae5400ce87c4aeab1d9244
Armel <adjepang@yahoo.fr>
2024-06-03 22:14:55 +0200

    recipe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './components/Main';
+import RecipeList from './components/RecipeList';
+import RecipeDetail from './components/RecipeDetail';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <Main>
+          <Switch>
+            <Route exact path="/" component={RecipeList} />
+            <Route path="/recipe/:id" component={RecipeDetail} />
+          </Switch>
+        </Main>
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
